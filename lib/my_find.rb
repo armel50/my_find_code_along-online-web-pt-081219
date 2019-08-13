60b0010fef14adbd6df92e1cc44c11d8baf3480b
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 15:55:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.size do 
+    if yield(collection[i])
+      found = 
+    end
    found = true if yield(collection[i])
     i += 1
   end 
